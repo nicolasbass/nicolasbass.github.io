3b24e855d1aa5ad6ee96a9817953d6db91ae932e
Nicolas Sierra <nicolas.sierra@tambourine.com>
2022-10-10 17:46:20 -0500

    Progress

---
+++ app.js
@@ -0,0 +1,25 @@
+let number = document.getElementById("cardNumber");
+let numberInput = document.getElementById("cardNumberInput");
+
+numberInput.onkeyup = (() => {
+    numberInput.value = cc_format(numberInput.value);
+    number.value = numberInput.value;
+})
+
+function cc_format(value) {
+    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+    var matches = v.match(/\d{4,16}/g);
+    var match = matches && matches[0] || ''
+    var parts = []
+
+    for (i=0, len=match.length; i<len; i+=4) {
+        parts.push(match.substring(i, i+4))
+    }
+
+    if (parts.length) {
+        return parts.join(' ')
+    } else {
+        return value
+    }
+}
+
