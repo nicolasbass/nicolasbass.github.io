0dd44e204351091972a3c5a0a7cbe5db177971d3
Nicolas Sierra <nicolas.sierra@tambourine.com>
2022-10-18 14:52:16 -0500

    Minor changes

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ function isFilled(i) {
     }
     if (input[i].value.length == 0) {
         input[i].setCustomValidity("Can't be blank");
-        isValidate = false;
+        isValid = false;
         if (i == 2 || i == 3) {
             document.querySelector(".date + .error").style.display = "block";
         }
     } else {
-        isValidate = true;
+        isValid = true;
         input[i].setCustomValidity("");
         validDate();
     }
@@ -62,7 +62,7 @@ for (let j = 0; j < input.length; j++) {
 
 let form = document.getElementById("form");
 
-let isValidate = true;
+let isValid = true;
 
 form.onsubmit = ((e) => {
     e.preventDefault();
@@ -71,7 +71,7 @@ form.onsubmit = ((e) => {
         isFilled(i);
     };
 
-    if (isValidate) {
+    if (isValid) {
 
         document.querySelector(".form-container").innerHTML = '<div class="status"> <figure> <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="40" cy="40" r="40" /><path d="M28 39.92 36.08 48l16-16" stroke-width="3"/><defs><linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse"><stop stop-color="#6348FE"/><stop offset="1" stop-color="#610595"/></linearGradient></defs></svg> </figure> <div class="thanks"> <h1> THANK YOU! <span>' + "We've added your card details </span>" + '</h1> <button class="btn"> Continue </button> </div> <div>';
 
